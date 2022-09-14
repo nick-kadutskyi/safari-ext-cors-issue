4f33a69f14663451b3489b304dc56c47a8066a1d
Nick Kadutskyi <nick@kadutskyi.com>
2022-09-13 22:55:15 -0700

    Initial Commit

---
+++ Test CORS Issue/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Test CORS Issue
+//
+//  Created by Nick Kadutskyi on 9/13/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
