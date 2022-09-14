0f859a89319d718fa5eefe13e3be9c90daef8770
Nick Kadutskyi <nick@kadutskyi.com>
2022-09-13 23:23:08 -0700

    Fetch calls

--- Test CORS Issue Extension/Resources/background.js
+++ Test CORS Issue Extension/Resources/background.js
@@ -1,6 +1,26 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 
-    if (request.greeting === "hello")
+    if (request.greeting === "hello"){
+        fetch("https://example.com")
+        .then(response=>{
+            console.log("Success 1", response)
+        })
+        .catch(error=>{
+            console.log("Fail 1", error)
+        })
         sendResponse({ farewell: "goodbye" });
+    }
 });
+
+browser.action.onClicked.addListener(()=>{
+    
+    fetch("https://example.com")
+    .then(response=>{
+        console.log("Success 2", response)
+    })
+    .catch(error=>{
+        console.log("Fail 2", error)
+    })
+    
+})
